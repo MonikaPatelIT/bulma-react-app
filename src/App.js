d8fa95e4b14cc06d6f4c8b22551022080993ecad
monika patel <monikapatel.it@outlook.com>
2018-12-04 14:41:35 +1300

    React-app with bulma frame work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bulma-start/css/main.css'
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <section className="hero is-primary is-medium">
+        <div className="hero-head">
+          <nav className="navbar">
+            <div className="container">
+              <div className="navbar-brand">
+                <a className="navbar-item">
+                  <img
+                    src="https://bulma.io/images/bulma-type-white.png"
+                    alt="Logo"
+                  />
+                </a>
+                <span
+                  className="navbar-burger burger"
+                  data-target="navbarMenuHeroA"
+                >
+                  <span />
+                  <span />
+                  <span />
+                </span>
+              </div></div>
+          </nav>
+        </div>
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <h1 className="title">Title</h1>
+            <h2 className="subtitle">Subtitle</h2>
+          </div>
+
+          <div className="hero-foot">
+          <nav className="tabs">
+            <div className="container">
+              <ul>
+                <li className="is-active">
+                  <a>Overview</a>
+                </li>
+                <li>
+                  <a>Modifiers</a>
+                </li>
+                <li>
+                  <a>Grid</a>
+                </li>
+                <li>
+                  <a>Elements</a>
+                </li>
+                <li>
+                  <a>Components</a>
+                </li>
+                <li>
+                  <a>Layout</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+        </div>
+      
+
+        </section>
+    </div>
     );
   }
 }
